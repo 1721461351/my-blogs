05bc50e94be7af13b0422ed6121b96e0869a9718
y-wang_wang <1721461351@qq.com>
2020-03-13 01:35:42 +0800

    搜索功能完善

--- src/main.js
+++ src/main.js
@@ -9,14 +9,32 @@ Vue.config.productionTip = false
 // 使用vueResource
 Vue.use(vueResource)
 
-
-Vue.directive('rainbox',{
-  bind(el,binding,vnode){
-    el.style.color="#"+Math.random().toString(16).slice(2,8)
-    console.log(Math.random().toString().slice(0,6))
+// 自定义指令
+Vue.directive('rainbox', {
+  bind(el, binding, vnode) {
+    el.style.color = "#" + Math.random().toString(16).slice(2, 8)
+    console.log(Math.random().toString().slice(0, 6))
   }
 })
+
+
+// 自定义过滤器
 /* eslint-disable no-new */
+/**
+ * 把标题的所有小写改成大写
+ */
+Vue.filter('toUppercase', (value) => {
+  // console.log('value:'+value)
+  return value.toUpperCase();
+})
+
+/***
+ * 把多余内容省略
+ */
+Vue.filter('omit',(value)=>{
+  console.log('value:'+value)
+  return value.slice(0,100)+'...';
+})
 new Vue({
   el: '#app',
   router,
